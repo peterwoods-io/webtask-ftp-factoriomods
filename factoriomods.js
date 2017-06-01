7c4a057f55567581ba7d409417c48fa3ea11bd55
Peter Woods <peter@invoy.com>
2017-05-31 19:32:15 -0700

    Add package.json and get ready to start rolling

--- factoriomods.js
+++ factoriomods.js
@@ -2,3 +2,12 @@
     Webtask to check whether Factorio mods on a given FTP server are up to date,
     and provide links to the latest versions if they're not.
 */
+
+'use strict';
+
+const FtpClient = require('ftp');
+const Https = require('https');
+
+module.exports = (ctx, cb) => {
+    cb(null, 'Done');
+};
